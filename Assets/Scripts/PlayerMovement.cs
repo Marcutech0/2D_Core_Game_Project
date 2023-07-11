7ab2b0eb2eabd10aa87624e1d9a042407a26cff1
Josh Guinto <134356678+Marcutech0@users.noreply.github.com>
2023-07-11 12:24:08 +0800

    deleted some comments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,52 +35,9 @@ public class PlayerMovement : MonoBehaviour
     // PLAY/LAUNCH
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) //move up
-        //{
-        //    anim.enabled = true;//
-        //    anim.SetTrigger("moveup");
-        //}
-        //if (Input.GetKeyUp(KeyCode.W))
-        //{
-        //    anim.SetTrigger("Upause");
-        //}
-        //if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))//move down
-        //{
-        //    anim.enabled = true;
-        //    anim.SetTrigger("movedown");
-        //}
-        //if (Input.GetKeyUp(KeyCode.S))
-        //{
-        //    anim.SetTrigger("Dpause");
-        //}
-        //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))//move to the right
-        //{
-        //    anim.enabled = true;
-        //    anim.SetTrigger("moveright");
-        //}
-        //if (Input.GetKeyUp(KeyCode.D))
-        //{
-        //    anim.SetTrigger("Rpause");
-        //}
-        //if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))//move to the right
-        //{
-        //    anim.enabled = true;
-        //    anim.SetTrigger("moveleft");
-        //}
-        //if (Input.GetKeyUp(KeyCode.A))
-        //{
-        //    anim.SetTrigger("Lpause");
-        //}
-        //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
-        //{
-        //    anim.enabled = false;
-        //}
-
         anim.SetFloat("Horizontal", movementInput.x);
         anim.SetFloat("Vertical", movementInput.y);
         anim.SetFloat("Speed", movementInput.sqrMagnitude);
-        
-
     }
     //Fixed for physx kimi lang pre
     private void FixedUpdate()
